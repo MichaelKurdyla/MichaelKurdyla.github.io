a88f4b576c34f8af51815f130a67fdc4dfe01a4b
Michael Kurdyla <michael.kurdyla@hotmail.com>
2019-08-15 21:27:58 -0400

    Change layout

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  Typed.new('#typed-text', {
+    strings: ["Computer Engineering Student", "Weight Lifter", "Excited to Learn"],
+    typeSpeed: 90,
+    backDelay:1000,
+    contentType: 'text',
+    loop: null
+  });
+});
