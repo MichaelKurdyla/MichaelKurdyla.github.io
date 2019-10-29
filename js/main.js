965db28fc09764801ddba1decdc7636110bc42e6
Michael Kurdyla <michael.kurdyla@hotmail.com>
2019-10-28 20:57:21 -0400

    Updated Typing options

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   Typed.new('#typed-text', {
-    strings: ["Computer Engineering Student", "Weight Lifter", "Excited to Learn"],
+    strings: ["Computer Engineering Student", "NeuroTech Enthusiast", "Network Operations Member at Queen's", "Weight Lifter", "Excited to Learn" ],
     typeSpeed: 90,
     backDelay:1000,
     contentType: 'text',
