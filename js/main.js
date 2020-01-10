0d46b7d527efa8d2b4965406deab96cb24e6f1aa
unknown <michael.kurdyla@hotmail.com>
2020-01-10 13:38:04 -0500

    Added Job to type animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   Typed.new('#typed-text', {
-    strings: ["Computer Engineering Student", "NeuroTech Enthusiast", "Network Operations Member at Queen's", "Weight Lifter", "Excited to Learn" ],
+    strings: ["Computer Engineering Student", "Cloud Software Developer @ Ericsson","NeuroTech Enthusiast", "Network Operations Member at Queen's", "Weight Lifter"],
     typeSpeed: 90,
     backDelay:1000,
     contentType: 'text',
