8df44d4cda979f2381d043dda6f47dd6c8a3a4c5
unknown <michael.kurdyla@hotmail.com>
2020-01-10 13:57:33 -0500

    Fix Speed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   Typed.new('#typed-text', {
     strings: ["Computer Engineering Student", "Cloud Software Developer @ Ericsson","NeuroTech Enthusiast", "Network Operations Member at Queen's", "Weight Lifter"],
-    typeSpeed: 90,
+    typeSpeed: 110,
     backDelay:1000,
     contentType: 'text',
     loop: null
