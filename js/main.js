4e9c346b53d4c702df7861f861a52794902a1a16
unknown <michael.kurdyla@hotmail.com>
2020-01-14 15:58:15 -0500

    uniform text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   Typed.new('#typed-text', {
-    strings: ["Computer Engineering Student", "Cloud Software Developer @ Ericsson","NeuroTech Enthusiast", "Network Operations Member at Queen's", "Weight Lifter"],
+    strings: ["Computer Engineering Student", "Cloud Software Developer @ Ericsson","NeuroTech Enthusiast", "Network Operations Member @ Queen's", "Weight Lifter"],
     typeSpeed: 110,
     backDelay:1000,
     contentType: 'text',
